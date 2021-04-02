8d27393913c2aec338aa0e25e4566a4cdfa07934
Kinzamalik <kiinza.malik@gmail.com>
2021-04-02 19:46:34 +0500

    dart fundamentals

---
+++ bin/main.dart
@@ -0,0 +1,28 @@
+void main() {
+  print('Hello dart from kinzay');
+
+  // Data types
+  // Variables
+  var name = "Kinza";
+  print(name);
+  // Integer
+  int num1 = 10;
+  print(num1);
+
+  // double
+  double num2 = 10.23;
+  print(num2);
+
+  // String
+  String city = "karachi";
+  print(city);
+
+  // Boolean
+  bool is_true = true;
+  print(is_true);
+
+  var quantity = 10;
+  print(quantity);
+  print(quantity.runtimeType);
+
+}
