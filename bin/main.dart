d04cd348e95fc04d6d36927bd95e03c39671c675
Kinzamalik <kiinza.malik@gmail.com>
2021-04-16 09:23:30 +0500

    paracticing dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,28 +1,49 @@
 void main() {
-  print('Hello dart from kinzay');
+  // print('Hello dart from kinzay');
 
-  // Data types
-  // Variables
-  var name = "Kinza";
-  print(name);
-  // Integer
-  int num1 = 10;
-  print(num1);
+  // // Data types
+  // // Variables
+  // var name = "Kinza";
+  // print(name);
+  // // Integer
+  // int num1 = 10;
+  // print(num1);
 
-  // double
-  double num2 = 10.23;
-  print(num2);
+  // // double
+  // double num2 = 10.23;
+  // print(num2);
 
-  // String
-  String city = "karachi";
-  print(city);
+  // // String
+  // String city = "karachi";
+  // print(city);
 
   // Boolean
-  bool is_true = true;
-  print(is_true);
+  // bool is_true = true;
+  // print(is_true);
 
-  var quantity = 10;
-  print(quantity);
-  print(quantity.runtimeType);
+  // var quantity = 10;
+  // print(quantity);
+  // Check data types
+
+  // print(quantity.runtimeType);
+  // 
+  // Arithmetic operators
+  // var a = 10;
+  // var b = 30;
+
+  // print(a+b);
+  // print(a*b);
+  // print(a-b);
+  // print(a+b);
+  // print(a~/b);
+  // print(a%b);
+  //
+  //
+  //prefix and postfix and Increment and Decrement Opererators
+  var i = 3;
+  print(i);
+  print(++i);
+  print(i);
+
 
 }
